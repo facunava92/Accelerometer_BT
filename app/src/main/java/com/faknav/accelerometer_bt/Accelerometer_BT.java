8810f54b098fc6e1c5250e7027471451906275bd
Facundo Navarro <facunava92@gmail.com>
2016-09-03 01:37:51 -0300

    se agrega funcionalidad de actividad bluetooth

--- app/src/main/java/com/faknav/accelerometer_bt/Accelerometer_BT.java
+++ app/src/main/java/com/faknav/accelerometer_bt/Accelerometer_BT.java
@@ -1,5 +1,6 @@
 package com.faknav.accelerometer_bt;
 
+
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,16 +11,79 @@ import android.content.Context;
 import android.os.Vibrator;
 import android.widget.TextView;
 
+
+/////Switch
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+///////Bluetooth
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.widget.Toast;
+import android.content.Intent;
+
+
 public class Accelerometer_BT extends Activity {
-    TextView textX, textY, textZ;
+    TextView textX, textY, textZ, text;
     SensorManager sensorManager;
     Sensor accelerometer;
     Vibrator v;
     float vibrateThreshold = 5;
 
+    Switch mySwitch;
+
+     BluetoothAdapter myBluetoothAdapter;
+    private static final int REQUEST_ENABLE_BT = 1;
+
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer__bt);
+
+        mySwitch = (Switch) findViewById(R.id.mySwitch);
+        text = (TextView) findViewById(R.id.text);
+
+        //set the switch to ON
+        mySwitch.setChecked(false);
+        // take an instance of BluetoothAdapter - Bluetooth radio
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(myBluetoothAdapter == null) {
+            text.setText("Status: not supported");
+            Toast.makeText(getApplicationContext(),"Your device does not support Bluetooth", Toast.LENGTH_LONG).show();
+        }
+
+        else {   //attach a listener to check for changes in state
+            mySwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked)
+                    {
+                        text.setText("Estado: Activado");
+                        if (!myBluetoothAdapter.isEnabled()) {
+                            Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                            startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
+
+                            Toast.makeText(getApplicationContext(), "Bluetooth turned on", Toast.LENGTH_LONG).show();
+                        } else
+                        {
+                            Toast.makeText(getApplicationContext(), "Bluetooth is already on", Toast.LENGTH_LONG).show();
+                        }
+
+
+                    }
+
+                    else{
+                        myBluetoothAdapter.disable();
+                        text.setText("Estado: Desactivado");
+
+                        Toast.makeText(getApplicationContext(),"Bluetooth turned off", Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+            });
+        }
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
@@ -38,6 +102,7 @@ public class Accelerometer_BT extends Activity {
         textY = (TextView) findViewById(R.id.textY);
         textZ = (TextView) findViewById(R.id.textZ);
     }
+
 
     //onResume() register the accelerometer for listening the events
     protected void onResume() {
