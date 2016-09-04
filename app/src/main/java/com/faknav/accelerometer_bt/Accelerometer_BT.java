ca1ee68239a4400433d82bfe9070b11ac2bae6cc
Facundo Navarro <facunava92@gmail.com>
2016-09-04 00:35:50 -0300

    avance BT

--- app/src/main/java/com/faknav/accelerometer_bt/Accelerometer_BT.java
+++ app/src/main/java/com/faknav/accelerometer_bt/Accelerometer_BT.java
@@ -1,6 +1,8 @@
 package com.faknav.accelerometer_bt;
 
 
+import android.content.IntentFilter;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -16,12 +18,16 @@ import android.widget.TextView;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.view.Menu;
 
 ///////Bluetooth
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.widget.Toast;
 import android.content.Intent;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 
 public class Accelerometer_BT extends Activity {
@@ -32,8 +38,10 @@ public class Accelerometer_BT extends Activity {
     float vibrateThreshold = 5;
 
     Switch mySwitch;
+    ListView myListView;
+    BluetoothAdapter myBluetoothAdapter;
+    ArrayAdapter<String> BTArrayAdapter;
 
-     BluetoothAdapter myBluetoothAdapter;
     private static final int REQUEST_ENABLE_BT = 1;
 
 
@@ -45,13 +53,13 @@ public class Accelerometer_BT extends Activity {
         mySwitch = (Switch) findViewById(R.id.mySwitch);
         text = (TextView) findViewById(R.id.text);
 
-        //set the switch to ON
+        //set the switch to OFF
         mySwitch.setChecked(false);
         // take an instance of BluetoothAdapter - Bluetooth radio
         myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(myBluetoothAdapter == null) {
             text.setText("Status: not supported");
-            Toast.makeText(getApplicationContext(),"Your device does not support Bluetooth", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(),"Tu Dispositivo no tiene soporte para Bluetooth", Toast.LENGTH_LONG).show();
         }
 
         else {   //attach a listener to check for changes in state
@@ -59,31 +67,26 @@ public class Accelerometer_BT extends Activity {
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                     if (isChecked)
                     {
-                        text.setText("Estado: Activado");
-                        if (!myBluetoothAdapter.isEnabled()) {
-                            Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                            startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
-
-                            Toast.makeText(getApplicationContext(), "Bluetooth turned on", Toast.LENGTH_LONG).show();
-                        } else
-                        {
-                            Toast.makeText(getApplicationContext(), "Bluetooth is already on", Toast.LENGTH_LONG).show();
-                        }
-
-
+                        ON();
+                        FIND();
+                        text.setText("Activado");
+                        text.setTextColor(Color.GREEN);
                     }
-
                     else{
-                        myBluetoothAdapter.disable();
-                        text.setText("Estado: Desactivado");
-
-                        Toast.makeText(getApplicationContext(),"Bluetooth turned off", Toast.LENGTH_LONG).show();
-
+                        OFF();
+                        text.setText("Desactivado");
+                        text.setTextColor(Color.RED);
+                        BTArrayAdapter.clear();
                     }
-
                 }
             });
         }
+
+        myListView = (ListView)findViewById(R.id.listView1);
+
+        // create the arrayAdapter that contains the BTDevices, and set it to the ListView
+        BTArrayAdapter = new ArrayAdapter<String>(this, R.layout.list_fak);
+        myListView.setAdapter(BTArrayAdapter);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
@@ -103,7 +106,45 @@ public class Accelerometer_BT extends Activity {
         textZ = (TextView) findViewById(R.id.textZ);
     }
 
+    public void ON(){
 
+
+        Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
+        Toast.makeText(getApplicationContext(), "Bluetooth Encendido", Toast.LENGTH_LONG).show();
+    }
+
+    public void OFF(){
+        myBluetoothAdapter.disable();
+
+        Toast.makeText(getApplicationContext(),"Bluetooth Apagado", Toast.LENGTH_LONG).show();
+    }
+
+    final BroadcastReceiver bReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // add the name and the MAC address of the object to the arrayAdapter
+                BTArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                BTArrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+
+    public void FIND() {
+
+            myBluetoothAdapter.startDiscovery();
+            registerReceiver(bReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(bReceiver);
+    }
     //onResume() register the accelerometer for listening the events
     protected void onResume() {
         super.onResume();
@@ -135,3 +176,5 @@ public class Accelerometer_BT extends Activity {
 
     };
 }
+
+
